c03fb18bc1bdfb16f23c6ff608795a1e28951ffe
0404070c <0404070c@student.gla.ac.uk>
2017-07-28 18:37:56 +0100

    added eureka server

--- weatherCollector/src/main/java/weatherCollector/Application.java
+++ weatherCollector/src/main/java/weatherCollector/Application.java
@@ -3,10 +3,11 @@ package weatherCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
 @EnableAutoConfiguration
 public class Application {
