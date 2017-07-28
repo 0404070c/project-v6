f1a9d61f5e851bdf63ed51af5252a9323f9433e6
0404070c <0404070c@student.gla.ac.uk>
2017-07-28 14:52:29 +0100

    3 stations, all collected and returned as json

--- weatherCollector/src/main/java/weatherCollector/WeatherController.java
+++ weatherCollector/src/main/java/weatherCollector/WeatherController.java
@@ -2,7 +2,6 @@ package weatherCollector;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,41 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class WeatherController {
-	
-	//private Random value = new Random(); 
-	//private int count = 1;
-	
-	//private int stationId = 100000 + value.nextInt(900000);
-	//private int temperature = 0 + value.nextInt(100);
-	//private int rain = 0 + value.nextInt(100);
-/*
-	@RequestMapping( 
-			method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            value="/"
-            )
-	public Weather weather() {
-        return new Weather(stationId, count++, temperature, rain);
-    }
-	*/
-	/*
-	@RequestMapping( 
-			method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            value="/"
-            )
-	public Weather weather() {
-		RestTemplate restTemplate = new RestTemplate();
-		Weather stationOne = restTemplate.getForObject("http://localhost:8080/", Weather.class);
-		
-		int idOne = stationOne.getStationId();
-	    int countOne = stationOne.getCount();
-	    int temperatureOne = stationOne.getTemperature();
-	    int rainOne = stationOne.getRain();
-	    
-		return new Weather(idOne, countOne, temperatureOne, rainOne);
-	}
-	*/
+
 	@RequestMapping( 
 			method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE,
@@ -56,15 +21,28 @@ public class WeatherController {
 	private List<Weather> collectWeather() {
 		RestTemplate restTemplate = new RestTemplate();
 		Weather stationOne = restTemplate.getForObject("http://localhost:8001/", Weather.class);
-		//Weather stationTwo = restTemplate.getForObject("http://localhost:8002/", Weather.class);
+		Weather stationTwo = restTemplate.getForObject("http://localhost:8002/", Weather.class);
+		Weather stationThree = restTemplate.getForObject("http://localhost:8003/", Weather.class);
 		
 		int idOne = stationOne.getStationId();
 	    int countOne = stationOne.getCount();
 	    int temperatureOne = stationOne.getTemperature();
 	    int rainOne = stationOne.getRain();
 	    
+	    int idTwo = stationTwo.getStationId();
+	    int countTwo = stationTwo.getCount();
+	    int temperatureTwo = stationTwo.getTemperature();
+	    int rainTwo = stationTwo.getRain();
+	    
+	    int idThree = stationThree.getStationId();
+	    int countThree = stationThree.getCount();
+	    int temperatureThree = stationThree.getTemperature();
+	    int rainThree = stationThree.getRain();
+	    
 		List<Weather> collection = new ArrayList<>();
 		collection.add(new Weather(idOne, countOne, temperatureOne, rainOne));
+		collection.add(new Weather(idTwo, countTwo, temperatureTwo, rainTwo));
+		collection.add(new Weather(idThree, countThree, temperatureThree, rainThree));
 	
 		return collection;
 	}
@@ -99,6 +77,42 @@ public class WeatherController {
 		resources.add(new Weather(stationId, count++, temperature, rain));
 	
 		return collection;
+	}
+	*/
+
+
+	//private Random value = new Random(); 
+	//private int count = 1;
+	
+	//private int stationId = 100000 + value.nextInt(900000);
+	//private int temperature = 0 + value.nextInt(100);
+	//private int rain = 0 + value.nextInt(100);
+	/*
+	@RequestMapping( 
+			method = RequestMethod.GET,
+	        produces = MediaType.APPLICATION_JSON_VALUE,
+	        value="/"
+	        )
+	public Weather weather() {
+	    return new Weather(stationId, count++, temperature, rain);
+	}
+	*/
+	/*
+	@RequestMapping( 
+			method = RequestMethod.GET,
+	        produces = MediaType.APPLICATION_JSON_VALUE,
+	        value="/"
+	        )
+	public Weather weather() {
+		RestTemplate restTemplate = new RestTemplate();
+		Weather stationOne = restTemplate.getForObject("http://localhost:8080/", Weather.class);
+		
+		int idOne = stationOne.getStationId();
+	    int countOne = stationOne.getCount();
+	    int temperatureOne = stationOne.getTemperature();
+	    int rainOne = stationOne.getRain();
+	    
+		return new Weather(idOne, countOne, temperatureOne, rainOne);
 	}
 	*/
 
---
+++ weatherStationOne/src/main/java/weatherStationOne/WeatherController.java
@@ -0,0 +1,29 @@
+package weatherStationOne;
+
+import java.util.Random;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class WeatherController {
+	
+	private int stationId = 1;
+	private Random value = new Random(); 
+	private int count = 1;
+	private int temperature = 0 + value.nextInt(100);
+	private int rain = 0 + value.nextInt(100);
+
+	@RequestMapping( 
+			method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            value="/"
+            )
+	public Weather weather() {
+        return new Weather(stationId, count++, temperature, rain);
+    }
+
+}
